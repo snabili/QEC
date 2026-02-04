f1556f532512cfbab341f0f9439d071932bc33a7
Sara Nabili <saranabili@Saras-MacBook-Air.local>
2026-02-03 21:33:48 -0500

    remove file

--- test/ibm_api.py
+++
@@ -1,3 +0,0 @@
-MY_TOKEN = "2rPT2TDj7rwxXkO6MPlb_LRpuTNx_TGgaV35GKcXuI8L"
-MY_CRN = "crn:v1:bluemix:public:quantum-computing:us-east:a/d31adb9840294bc1ac19f535cc72f970:7e56e841-a924-4987-ba2e-0592743f8f3b::" # Cloud Resource Name
-
